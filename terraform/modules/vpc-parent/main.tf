8c4e0cae5ad103da3e7313d4283f9ca429261fa4
krishnak <krishna.dc@anz.com>
2021-04-30 21:57:02 +0530

    Fixing module path

--- terraform/modules/vpc-parent/main.tf
+++ terraform/modules/vpc-parent/main.tf
@@ -1,11 +1,11 @@
 resource "google_compute_network" "network" {
-  name                            = var.network_name
-  auto_create_subnetworks         = var.auto_create_subnetworks
-  routing_mode                    = var.routing_mode
-  project                         = var.project_id
-  description                     = var.description
-  delete_default_routes_on_create = var.delete_default_internet_gateway_routes
-  mtu                             = var.mtu
+  name                            = "${var.network_name}"
+  auto_create_subnetworks         = "${var.auto_create_subnetworks}"
+  routing_mode                    = "${var.routing_mode}"
+  project                         = "${var.project_id}"
+  description                     = "${var.description}"
+  delete_default_routes_on_create = "${var.delete_default_internet_gateway_routes}"
+  mtu                             = "${var.mtu}"
 }
 
 /******************************************
@@ -14,7 +14,7 @@ resource "google_compute_network" "network" {
 resource "google_compute_shared_vpc_host_project" "shared_vpc_host" {
   provider = google-beta
 
-  count      = var.shared_vpc_host ? 1 : 0
-  project    = var.project_id
+  count      = "${var.shared_vpc_host} ? 1 : 0"
+  project    = "${var.project_id}"
   depends_on = [google_compute_network.network]
 }
