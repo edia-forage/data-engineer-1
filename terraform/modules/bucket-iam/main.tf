644d87bc5df846a465126cd3fb831a4ba6d2235b
krishnak <krishna.dc@anz.com>
2021-05-01 10:55:35 +0530

    fixing v0.13 syntax

--- terraform/modules/bucket-iam/main.tf
+++ terraform/modules/bucket-iam/main.tf
@@ -2,7 +2,7 @@ resource "google_storage_bucket_iam_binding" "binding" {
   count   = "${var.set_iam_flag == "0" ? 0 : length(var.members) == 0 ? 0 : var.authoritative == "true" ? 1 : 0}"
   bucket  = "${var.bucket_name}"
   role    = "${var.role_id}"
-  members = ["${var.members}"]
+  members = "${var.members}"
 }
 
 resource "google_storage_bucket_iam_member" "member" {
